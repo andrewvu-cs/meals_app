52e4f18e7773f879f671a6e1a327fc5604ad3e11
Andrew <andrewvu.cs@gmail.com>
2019-10-19 12:57:22 -0700

    added categories data to a flatlist on categories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,40 @@
 // Screen holds the nationality of food
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = props => {
-    return(<View style={styles.screen}>
-        <Text>The CategoriesScreen!</Text>
-    </View>)
+  console.log(props);
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
+  }
 });
 
 export default CategoriesScreen;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,14 @@
 // Screen holds the nationality of food
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoryMealScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The CategoyMealsScreen!</Text>
+      <Button title="GO to MEAL DETAILS" onPress={()=>{
+        props.navigation.navigate('MealDetails')
+      }}/>
     </View>
   );
 };
